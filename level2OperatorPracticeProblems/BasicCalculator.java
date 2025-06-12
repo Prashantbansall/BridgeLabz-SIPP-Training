e3e6f0343682d927ce622f33ac1c2eb1a7abb95c
Prashant Bansal <114644999+Prashantbansall@users.noreply.github.com>
2025-06-12 08:55:10 +0530

    Add files via upload

---
+++ level2OperatorPracticeProblems/BasicCalculator.java
@@ -0,0 +1,26 @@
+package level2PracticeProblems;
+import java.util.*;
+
+public class BasicCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double number1 = input.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double number2 = input.nextDouble();
+
+        double sum = number1 + number2;
+        double difference = number1 - number2;
+        double product = number1 * number2;
+        double quotient = number1 / number2;
+
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers " + number1 + " and " + number2 + " is " + sum + ", " + difference + ", " + product + ", and " + quotient + ".");
+
+        input.close();
+	}
+
+}
