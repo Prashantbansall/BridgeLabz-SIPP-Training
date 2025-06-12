aa36ddd0a7c73547326afcf268161cc2986afef6
Prashant Bansal <114644999+Prashantbansall@users.noreply.github.com>
2025-06-12 08:52:26 +0530

    Add files via upload

---
+++ Operators-Problems/CurrentYearAge.java
@@ -0,0 +1,14 @@
+package operators;
+
+public class CurrentYearAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int birthYear = 2000;
+        int currentYear = 2024;
+        int age = currentYear - birthYear;
+        System.out.println("Harry's age in 2024 is " + age);
+		
+	}
+
+}
---
+++ Operators-Problems/HeightConversion.java
@@ -0,0 +1,23 @@
+package operators;
+
+import java.util.*;
+
+public class HeightConversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+        System.out.print("height in centimeters: ");
+        double heightCm = input.nextDouble();
+
+        double totalInches = heightCm / 2.54;
+        int feet = (int) (totalInches / 12);
+        double inches = totalInches % 12;
+
+        System.out.println("Your Height in cm is " + heightCm + " while in feet is " + feet + " and inches is " + inches);
+
+        input.close();
+	}
+
+}
