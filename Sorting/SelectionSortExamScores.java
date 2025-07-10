818e59bfa703d88732261a8f2ee14b8aacda0cb0
Prashant Bansal <114644999+Prashantbansall@users.noreply.github.com>
2025-07-10 12:13:09 +0530

    Update SelectionSortExamScores.java

--- Sorting/SelectionSortExamScores.java
+++ Sorting/SelectionSortExamScores.java
@@ -1,3 +1,11 @@
+// Selection Sort:- Sort Exam Scores
+// Problem Statement:- A university needs to sort students’ exam scores in ascending order. Implement Selection Sort to achieve this.
+// Hint:-
+// Find the minimum element in the array.
+// Swap it with the first unsorted element.
+// Repeat the process for the remaining elements.
+
+
 package BubbleSort;
 import java.util.Scanner;
 
