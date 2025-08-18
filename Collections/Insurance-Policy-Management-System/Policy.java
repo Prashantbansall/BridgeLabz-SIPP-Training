617e7d2baef57fbe46ef58dfe89e91e0daa26341
Prashant Bansal <114644999+Prashantbansall@users.noreply.github.com>
2025-08-18 14:03:27 +0530

    Create Policy.java

---
+++ Collections/Insurance-Policy-Management-System/Policy.java
@@ -0,0 +1,68 @@
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Policy implements Comparable<Policy> {
+    private String policyNumber;    // Unique identifier
+    private String policyholderName;
+    private LocalDate expiryDate;
+    private String coverageType;
+    private double premiumAmount;
+
+    public Policy(String policyNumber, String policyholderName, LocalDate expiryDate, String coverageType, double premiumAmount) {
+        this.policyNumber = policyNumber;
+        this.policyholderName = policyholderName;
+        this.expiryDate = expiryDate;
+        this.coverageType = coverageType;
+        this.premiumAmount = premiumAmount;
+    }
+
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public String getPolicyholderName() {
+        return policyholderName;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public String getCoverageType() {
+        return coverageType;
+    }
+
+    public double getPremiumAmount() {
+        return premiumAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Policy{" +
+                "policyNumber='" + policyNumber + '\'' +
+                ", policyholderName='" + policyholderName + '\'' +
+                ", expiryDate=" + expiryDate +
+                ", coverageType='" + coverageType + '\'' +
+                ", premiumAmount=" + premiumAmount +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Policy)) return false;
+        Policy policy = (Policy) o;
+        return Objects.equals(policyNumber, policy.policyNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(policyNumber);
+    }
+
+    // Used for sorting in TreeSet by expiry date
+    @Override
+    public int compareTo(Policy other) {
+        return this.expiryDate.compareTo(other.expiryDate);
+    }
+}
