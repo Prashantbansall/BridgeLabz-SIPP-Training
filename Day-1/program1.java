e702705fb2d95b42bf778d808e63e9bf052c49f4
Prashant Bansal <114644999+Prashantbansall@users.noreply.github.com>
2025-08-25 12:10:43 +0530

    Create program1.java

---
+++ Day-1/program1.java
@@ -0,0 +1,11 @@
+Q1. Undo Feature in Text Editor
+
+MAIN:
+    CREATE empty Stack textStack
+    PUSH "Hello" into stack
+    PUSH "Hello World" into stack
+    PUSH "Hello World!" into stack
+    PRINT top of stack (peek)
+
+    POP last element from stack (undo)
+    PRINT top of stack (peek) again
