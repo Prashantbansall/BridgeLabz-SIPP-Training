b084341562a05b57c73f479fe7374b6bab7f7aba
Prashant Bansal <114644999+Prashantbansall@users.noreply.github.com>
2025-08-28 12:05:05 +0530

    Delete Stream-API directory

--- Stream-API/Stream-API/forEach-Method-Problems/EventAttendeeWelcomeMessage/AttendeeWelcome.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class AttendeeWelcome {
-    public static void main(String[] args) {
-        List<String> attendees = Arrays.asList("Prashant", "Amit", "Sonia");
-
-        attendees.forEach(name -> System.out.println("Welcome, " + name + "!"));
-    }
-}
--- Stream-API/Stream-API/forEach-Method-Problems/StockPriceLogger/StockPriceLogger.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class StockPriceLogger {
-    public static void main(String[] args) {
-        List<Double> stockPrices = Arrays.asList(101.5, 103.2, 99.7, 104.8);
-        stockPrices.forEach(price -> System.out.println("Stock Price: " + price));
-    }
-}
